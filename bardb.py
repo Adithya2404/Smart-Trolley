4749f74d2b4b809d0b0ef921f5d8e800850da7c3
Adithya2404 <adithya.narayan2404@gmail.com>
2020-06-26 01:16:07 +0530

    classes defined

--- bardb.py
+++ bardb.py
@@ -2,34 +2,39 @@ import sqlite3
 conn = sqlite3.connect('testdb.db')
 c = conn.cursor()
 
-def initiate(BARCODE_DB):
+class db_mgt:
 
-    check_table = (""" SELECT count(name) FROM sqlite_master WHERE type='table' AND name='BARCODE_DB' """)
-    c.execute(check_table)
+    def __init__(self):
+        
+
+    def initiate(BARCODE_DB):
+
+        check_table = (""" SELECT count(name) FROM sqlite_master WHERE type='table' AND name='BARCODE_DB' """)
+        c.execute(check_table)
     
-    if c.fetchone()[0]==1 :
-        print('Table exists')
-    else:
-        create_table = (''' CREATE TABLE BARCODE_DB 
+        if c.fetchone()[0]==1 :
+            print('Table exists')
+        else:
+            create_table = (''' CREATE TABLE BARCODE_DB 
                ([Barcode] INTEGER PRIMARY KEY, [Product] text, [Price] integer)''')
-        c.execute(create_table)
+            c.execute(create_table)
 
-barcode = input('Barcode :')
-pdt_name = input('Product Name: ')
-amt = input('Price : ')
+# barcode = input('Barcode :')
+# pdt_name = input('Product Name: ')
+# amt = input('Price : ')
 
 ##insert function for inserting values
-# try:
-def barcode_check(barcode):
-    typeVar = str(barcode)
-    check_barcode = ("SELECT * from BARCODE_DB WHERE Barcode={}".format(typeVar))
-    c.execute(check_barcode)
-    codes = c.fetchall()
-    print(codes)
-   
-def update_db(barcode,pdt_name,amt):
-    insert_data = (""" INSERT INTO BARCODE_DB (Barcode, Product, Price) VALUES (?, ?, ?)""", (barcode, pdt_name, amt))
-    c.execute(insert_data)
+
+    def barcode_check(barcode):
+        typeVar = str(barcode)
+        check_barcode = ("SELECT * from BARCODE_DB WHERE Barcode={}".format(typeVar))
+        c.execute(check_barcode)
+        codes = c.fetchall()
+        print(codes)
+
+    def update_db(barcode,pdt_name,amt):
+        insert_data = (""" INSERT INTO BARCODE_DB (Barcode, Product, Price) VALUES (?, ?, ?)""", (barcode, pdt_name, amt))
+        c.execute(insert_data)
 
 initiate()
 barcode_check()

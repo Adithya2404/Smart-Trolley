244d1302e84d6c97db87f06dd5bb9ba6aa46f993
Adithya2404 <adithya.narayan2404@gmail.com>
2020-07-01 10:17:56 +0530

    minor changes

--- bardb.py
+++ bardb.py
@@ -5,20 +5,20 @@ c = conn.cursor()
 class db_mgt:
 
     def __init__(self):
-        self.conn
-        self.c
+        self.conn = sqlite3.connect('testdb.db')
+        self.c = conn.cursor()
 
-    def initiate(BARCODE_DB):
+    def initiate(self,BARCODE_DB):
 
         check_table = (""" SELECT count(name) FROM sqlite_master WHERE type='table' AND name='BARCODE_DB' """)
-        c.execute(check_table)
+        self.c.execute(check_table)
     
-        if c.fetchone()[0]==1 :
+        if self.c.fetchone()[0]==1 :
             print('Table exists')
         else:
             create_table = (''' CREATE TABLE BARCODE_DB 
                ([Barcode] INTEGER PRIMARY KEY, [Product] text, [Price] integer, [Weight] integer)''')
-            c.execute(create_table)
+            self.c.execute(create_table)
 
 # barcode = input('Barcode :')
 # pdt_name = input('Product Name: ')
@@ -27,18 +27,18 @@ class db_mgt:
     def barcode_check(barcode):
         typeVar = str(barcode)
         check_barcode = ("SELECT * from BARCODE_DB WHERE Barcode={}".format(typeVar))
-        c.execute(check_barcode)
-        codes = c.fetchall()
+        self.c.execute(check_barcode)
+        codes = self.c.fetchall()
         print(codes)
 
     def insert_db(barcode,pdt_name,amt,wt):
         insert_data = (""" INSERT INTO BARCODE_DB (Barcode, Product, Price, Weight) VALUES (?, ?, ?, ?)""", (barcode, pdt_name, amt,wt))
-        c.execute(insert_data)
+        self.c.execute(insert_data)
 
 
 
 obj = db_mgt()
-obj.insert(barcode,pdt_name,amt,wt)
+obj.insert_db(barcode,pdt_name,amt,wt)
 conn.commit()
 print ('Data entered successfully')
 conn.close()
